9079cf196ebe64ff39231999dbdf4203830db33c
DanielAsler <ale21007@byui.edu>
2021-12-05 01:03:36 +0100

    Score

---
+++ project/game/score.py
@@ -0,0 +1,20 @@
+import arcade
+import math
+from game.towers import Towers 
+from game import constants
+from game.zombie import Zombie
+from game.wizard import Wizard
+from game.side_menu import SideMenu
+from game.waves import Waves
+from game.fading_view import FadingView
+
+class KeepScore(FadingView):
+    """Class to keep the score"""
+    self.gui_camera = None
+    self.score = 0
+
+    self.gui_camera = arcade.Camera(self.width, self.height)
+
+    def on_draw(self):
+        """Remder the screen"""
+  

b4bfbfcb8b20d484537761a43365f8cb8998fa07
DanielAsler <ale21007@byui.edu>
2021-12-05 01:06:57 +0100

    da

--- project/game/score.py
+++ project/game/score.py
@@ -8,7 +8,7 @@ from game.side_menu import SideMenu
 from game.waves import Waves
 from game.fading_view import FadingView
 
-class KeepScore(FadingView):
+class KeepScore(self):
     """Class to keep the score"""
     self.gui_camera = None
     self.score = 0
